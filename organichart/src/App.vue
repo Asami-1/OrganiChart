03838b457505c1b2f0fb6343a5a11ee75ea4ec3d
GreenWojack <teddy78711@gmail.com>
2021-02-17 15:03:24 +0100

    ajout navbar

--- organichart/src/App.vue
+++ organichart/src/App.vue
@@ -1,15 +1,19 @@
 
 <template>
   <div id="app">
+    <Navbar> </Navbar>
     <img alt="Vue logo" src="./assets/logo.png" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Navbar
+  },
 };
 </script>
 
