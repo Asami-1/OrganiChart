4807f009c4b9773087ccaba48037ebf39c1dd1aa
GreenWojack <teddy78711@gmail.com>
2021-02-26 19:31:13 +0100

    Button add employee

--- organichart/src/components/EmployeeList.vue
+++ organichart/src/components/EmployeeList.vue
@@ -4,9 +4,13 @@
     <span class="form">
       <input type="text" v-model="search" placeholder="Search title.." />
     </span>
+    <div class="Add">
+      <button class="Add" v-on:click="AddEmployee()">+</button>
+    </div>
   </div>
 </template>
 <script>
+/* eslint-disable */ function AddEmployee() {}
 export default {
   name: 'EmployeeList',
 };
@@ -41,6 +45,10 @@ export default {
 input {
   height: 30px;
   width: 180px;
+  &:focus {
+    outline: none;
+    transform: scale(1.05);
+  }
 }
 .form {
   height: 30px;
@@ -59,5 +67,18 @@ span:after {
   width: 20px;
   background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 25 25' fill-rule='evenodd'%3E%3Cpath d='M16.036 18.455l2.404-2.405 5.586 5.587-2.404 2.404zM8.5 2C12.1 2 15 4.9 15 8.5S12.1 15 8.5 15 2 12.1 2 8.5 4.9 2 8.5 2zm0-2C3.8 0 0 3.8 0 8.5S3.8 17 8.5 17 17 13.2 17 8.5 13.2 0 8.5 0zM15 16a1 1 0 1 1 2 0 1 1 0 1 1-2 0'%3E%3C/path%3E%3C/svg%3E")
     center / contain no-repeat;
+}
+.test {
+  position: absolute;
+  top: 200px;
+  height: 10%;
+  background: red;
+}
+.Add {
+  height: 60px;
+  width: 60px;
+  position: fixed;
+  bottom: 20px;
+  border-radius: 50%;
 }
 </style>
