6eacdfbe748c2f60978b7b240e1e9efae2875488
GreenWojack <teddy78711@gmail.com>
2021-02-27 10:12:38 +0100

    modif bouton Add

--- organichart/src/components/EmployeeList.vue
+++ organichart/src/components/EmployeeList.vue
@@ -2,9 +2,13 @@
   <div class="List">
     <div class="employees">Employés</div>
     <span class="form">
-      <input type="text" v-model="search" placeholder="Search title.." />
+      <input
+        type="text"
+        v-model="search"
+        placeholder="Recherher un employé ..."
+      />
     </span>
-    <div class="Add">
+    <div>
       <button class="Add" v-on:click="AddEmployee()">+</button>
     </div>
   </div>
@@ -41,12 +45,12 @@ export default {
   font-weight: bold;
   padding: 3%;
   border: 1px solid black;
+  overflow: hidden;
 }
 input {
   height: 30px;
   width: 180px;
   &:focus {
-    outline: none;
     transform: scale(1.05);
   }
 }
@@ -56,7 +60,7 @@ input {
   top: 80px;
 }
 span {
-  position: relative;
+  width: 12rem;
 }
 span:after {
   content: '';
@@ -68,17 +72,17 @@ span:after {
   background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 25 25' fill-rule='evenodd'%3E%3Cpath d='M16.036 18.455l2.404-2.405 5.586 5.587-2.404 2.404zM8.5 2C12.1 2 15 4.9 15 8.5S12.1 15 8.5 15 2 12.1 2 8.5 4.9 2 8.5 2zm0-2C3.8 0 0 3.8 0 8.5S3.8 17 8.5 17 17 13.2 17 8.5 13.2 0 8.5 0zM15 16a1 1 0 1 1 2 0 1 1 0 1 1-2 0'%3E%3C/path%3E%3C/svg%3E")
     center / contain no-repeat;
 }
-.test {
-  position: absolute;
-  top: 200px;
-  height: 10%;
-  background: red;
-}
 .Add {
   height: 60px;
   width: 60px;
   position: fixed;
-  bottom: 20px;
+  bottom: 5%;
   border-radius: 50%;
+  background-color: #ffffff;
+  border: 1px solid black;
+  font-size: 120%;
+  &:focus {
+    transform: scale(1.05);
+  }
 }
 </style>
