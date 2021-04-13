2e7c333c2272134c9451f32d2e9eaa9c8bf4b134
Your Name <sofiane.abadou@insa-rouen.fr>
2021-04-13 11:40:19 +0200

    setup employee card and link with db

---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="card">
+    <div class="card-left">
+      <span>{{ this.employeeSurname }}</span>
+      <span>{{ this.employeeName }}</span>
+      <span>{{ this.employeePost }}</span>
+    </div>
+    <div class="card-right">
+      <button @click="deleteEmployee">Activate it !</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// import VueSimpleAlert from 'vue-simple-alert';
+import api from '../api';
+export default {
+  name: 'employeeCard',
+  data() {
+    return {};
+  },
+  methods: {
+    /**
+     * deletes an Employee
+     */
+    deleteEmployee() {
+      console.log(this.employeeId);
+      api.deleteEmployee(this.employeeId);
+    },
+  },
+  props: {
+    employeeId: {
+      type: Number,
+    },
+
+    employeeName: {
+      type: String,
+    },
+
+    employeeSurname: {
+      type: String,
+    },
+
+    //TODO Ajouter le nom du taf du salarié
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 80%;
+  height: 10%;
+  padding: 1vw;
+  background-color: white;
+
+  display: flex;
+  flex-direction: row;
+
+  &-left {
+    width: 80%;
+  }
+
+  &-right {
+    width: 20%;
+  }
+}
+</style>
--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -1,27 +1,66 @@
 <template>
-  <div class="List">
-    <div class="employees">Employés</div>
-    <span class="form">
-      <input
-        type="text"
-        v-model="search"
-        placeholder="Recherher un employé ..."
-      />
-    </span>
-    <div>
-      <button class="Add" v-on:click="AddEmployee()">+</button>
+  <div class="list">
+    <div class="list-header">
+      <div class="list-header-inside">
+        Employés
+        <div class="list-header-searchBar">
+          <input
+            type="text"
+            v-model="search"
+            placeholder="Recherher un employé ..."
+          />
+        </div>
+      </div>
     </div>
+
+    <div class="list-center">
+      <EmployeeCard
+        v-for="employee in employees"
+        :key="employee.employeeId"
+        :employee-id="employee.employeeId"
+        :employee-name="employee.name"
+        :employee-surname="employee.surname"
+      />
+    </div>
+    <!-- 
+    <div class="list-bottom">
+      <button class="Add" v-on:click="AddEmployee()">+</button>
+    </div> -->
   </div>
 </template>
+
+
 <script>
+import { db } from '../firebase';
+import 'firebase/firestore';
+import EmployeeCard from './EmployeeCard';
+
 //TODO : fix affichage bouton
 /* eslint-disable */ function AddEmployee() {}
 export default {
   name: 'EmployeeList',
+
+  data() {
+    return {
+      employees: null,
+      services: null,
+    };
+  },
+
+  firestore() {
+    return {
+      employees: db.collection('/Employees'),
+      services: db.collection('/Services'),
+    };
+  },
+  methods: {},
+  components: {
+    EmployeeCard,
+  },
 };
 </script>
 <style lang="scss" scoped>
-.List {
+.list {
   width: 20%;
   height: 100%;
   position: fixed;
@@ -33,57 +72,83 @@ export default {
   border: 1px solid black;
   display: flex;
   justify-content: center;
-  overflow-y: scroll;
-}
-.employees {
-  width: 50%;
-  background-color: #ffffff;
-  display: inline-flex;
-  top: 10px;
-  border-radius: 15%;
-  position: absolute;
-  justify-content: center;
-  font-weight: bold;
-  padding: 3%;
-  border: 1px solid black;
-  overflow: hidden;
-}
-input {
-  height: 30px;
-  width: 180px;
-  &:focus {
-    transform: scale(1.05);
+  align-items: center;
+  flex-direction: column;
+
+  &-header {
+    width: 100%;
+    height: 10%;
+    top: 0px;
+    display: inline-flex;
+    position: absolute;
+    justify-content: center;
+
+    &-inside {
+      width: 80%;
+      height: 80%;
+      background-color: #ffffff;
+      display: inline-flex;
+      top: 10px;
+      position: absolute;
+      border-radius: 15%;
+      justify-content: center;
+      font-weight: bold;
+      padding: 3%;
+      border: 1px solid black;
+      overflow: hidden;
+    }
+    &-searchBar {
+      position: absolute;
+      height: 50%;
+      width: 100%;
+      bottom: 0px;
+    }
   }
-}
-.form {
-  height: 30px;
-  position: absolute;
-  top: 80px;
-}
-span {
-  width: 12rem;
-}
-span:after {
-  content: '';
-  position: absolute;
-  top: 3px;
-  bottom: 0;
-  left: 85%;
-  width: 20px;
-  background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 25 25' fill-rule='evenodd'%3E%3Cpath d='M16.036 18.455l2.404-2.405 5.586 5.587-2.404 2.404zM8.5 2C12.1 2 15 4.9 15 8.5S12.1 15 8.5 15 2 12.1 2 8.5 4.9 2 8.5 2zm0-2C3.8 0 0 3.8 0 8.5S3.8 17 8.5 17 17 13.2 17 8.5 13.2 0 8.5 0zM15 16a1 1 0 1 1 2 0 1 1 0 1 1-2 0'%3E%3C/path%3E%3C/svg%3E")
-    center / contain no-repeat;
-}
-.Add {
-  height: 60px;
-  width: 60px;
-  position: fixed;
-  bottom: 5%;
-  border-radius: 50%;
-  background-color: #ffffff;
-  border: 1px solid black;
-  font-size: 120%;
-  &:focus {
-    transform: scale(1.05);
+
+  &-center {
+    overflow: hidden;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 20%;
+    height: 75%;
+    width: 100%;
+  }
+  &-bottom {
+    height: 20%;
+    width: 100%;
+    position: fixed;
+    bottom: 10px;
+    border-radius: 50%;
+    background-color: #ffffff;
+    border: 1px solid black;
+    font-size: 120%;
+    &:focus {
+      transform: scale(1.05);
+    }
+  }
+
+  input {
+    height: 30px;
+    width: 180px;
+    &:focus {
+      transform: scale(1.05);
+    }
+  }
+
+  span {
+    width: 12rem;
+  }
+  span:after {
+    content: '';
+    position: absolute;
+    top: 3px;
+    bottom: 0;
+    left: 85%;
+    width: 20px;
+    background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='25' height='25' viewBox='0 0 25 25' fill-rule='evenodd'%3E%3Cpath d='M16.036 18.455l2.404-2.405 5.586 5.587-2.404 2.404zM8.5 2C12.1 2 15 4.9 15 8.5S12.1 15 8.5 15 2 12.1 2 8.5 4.9 2 8.5 2zm0-2C3.8 0 0 3.8 0 8.5S3.8 17 8.5 17 17 13.2 17 8.5 13.2 0 8.5 0zM15 16a1 1 0 1 1 2 0 1 1 0 1 1-2 0'%3E%3C/path%3E%3C/svg%3E")
+      center / contain no-repeat;
   }
 }
 </style>
