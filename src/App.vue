71028cf96c341ea5ecb31c81c6923c1ff82b14e3
Your Name <sofiane.abadou@insa-rouen.fr>
2021-04-26 14:03:06 +0200

    added bar to select service

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,80 @@
   <div id="app">
     <Navbar> </Navbar>
     <EmployeeList></EmployeeList>
-    <img alt="Vue logo" src="./assets/logo.png" />
     <router-view></router-view>
+    <vue-dropdown
+      @click="updateSelectableServices"
+      @setSelectedOption="updateSelectedService($event)"
+      :config="config"
+      :key="dropdownKey"
+    ></vue-dropdown>
+    <button @click="print"></button>
   </div>
 </template>
 
 <script>
+import { db } from './firebase';
+import 'firebase/firestore';
 import Navbar from './components/Navbar';
 import EmployeeList from './components/EmployeeList';
+import VueDropdown from 'vue-dynamic-dropdown';
+
 export default {
   name: 'App',
   components: {
     Navbar,
     EmployeeList,
+    VueDropdown,
+  },
+
+  firestore() {
+    return {
+      employees: db.collection('/Employees'),
+      services: db.collection('/Services'),
+    };
+  },
+
+  methods: {
+    updateSelectableServices() {
+      console.log('hi');
+    },
+    updateSelectedService($event) {
+      console.log($event.value);
+      this.config.prefix = $event.value;
+    },
+
+    print() {
+      console.log('hi');
+    },
+  },
+
+  computed: {
+    servicesSelectable() {
+      return this.services;
+    },
+  },
+
+  watch: {
+    servicesSelectable() {
+      this.config.options = [];
+      this.services.forEach((service) => {
+        this.config.options.push({ value: service.id });
+      });
+      this.dropdownKey += 1;
+    },
+  },
+  data() {
+    return {
+      dropdownKey: 0,
+      employees: [],
+      services: [],
+      config: {
+        options: [],
+        prefix: 'Sélectionner un service',
+        backgroundColor: 'green',
+        width: 400,
+      },
+    };
   },
 };
 </script>
