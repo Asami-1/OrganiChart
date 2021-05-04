fcb497d398b49e289e849c1b543164b8ac309713
Your Name <sofiane.abadou@insa-rouen.fr>
2021-05-04 13:31:19 +0200

    added vuex store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import { firestorePlugin } from 'vuefire';
 import router from './router';
 import App from './App.vue';
+import store from './store';
+
 
 Vue.config.productionTip = false;
 Vue.use(firestorePlugin);
@@ -10,4 +12,15 @@ new Vue({
   el: '#app',
   router,
   render: (h) => h(App),
+  store,
+
+
+  created() {
+    setTimeout(() => {
+      store.dispatch('updateStore');
+
+    }, 5000);
+
+  },
 }).$mount('#app');
+
---
+++ src/store.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import api from './api';
+
+Vue.use(Vuex);
+
+/* eslint-disable no-param-reassign */
+/**
+ * Creates a global store accessible from everywhere in teh
+ */
+const store = new Vuex.Store({
+    state: {
+        services: {
+            posts: {},
+        },
+        employees: {},
+    },
+    mutations: {
+        updateEmployees(state, newEmployees) {
+            state.employees = newEmployees;
+        },
+
+        updateServices(state, newServices) {
+            state.services = newServices;
+        }
+
+    },
+    actions: {
+
+        async updateStore({ commit }) {
+            const employees = await api.getEmployees();
+            console.log(employees)
+            commit("updateEmployees", (employees));
+
+            const services = await api.getServices();
+            console.log(services)
+            commit('updateServices', services);
+
+        },
+
+
+
+    },
+});
+/* eslint-enable no-param-reassign */
+
+export default store;
