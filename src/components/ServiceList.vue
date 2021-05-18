7907f959888628ea0de26c2f0305b434ddb7efef
Your Name <sofiane.abadou@insa-rouen.fr>
2021-05-18 17:03:20 +0200

    Dropdown fixed

--- src/components/ServiceList.vue
+++ src/components/ServiceList.vue
@@ -2,31 +2,32 @@
   <div class="services">
     <div class="services-header">
       <vue-dropdown
-        @click="updateSelectableServices"
         @setSelectedOption="updateSelectedService($event)"
         :config="config"
         :key="dropdownKey"
       ></vue-dropdown>
     </div>
     <div class="services-body">
-      <Service></Service>
+      <Service
+        v-for="service in services"
+        :key="service.serviceName"
+        :service-name="service.serviceName"
+      ></Service>
     </div>
   </div>
 </template>
 
 <script>
+//TODO: Add possibility to add a service on the header
 import VueDropdown from 'vue-dynamic-dropdown';
 import Service from './Service';
 
-import { db } from '../firebase';
-import 'firebase/firestore';
 export default {
   name: 'ServiceList',
   data() {
     return {
+      // Necessary to fix a key to the dropdown
       dropdownKey: 0,
-      employees: [],
-      services: [],
       config: {
         options: [],
         prefix: 'Sélectionner un service',
@@ -40,34 +41,27 @@ export default {
     Service,
   },
 
-  firestore() {
-    return {
-      employees: db.collection('/Employees'),
-      services: db.collection('/Services'),
-    };
-  },
-
   methods: {
     updateSelectedService($event) {
       this.config.prefix = $event.value;
     },
-
-    print() {
-      console.log('hi');
-    },
   },
 
   computed: {
+    // this is temporary for debugging purpose
+    services() {
+      return this.$store.state.services;
+    },
     servicesSelectable() {
       return this.services;
     },
   },
 
   watch: {
-    servicesSelectable() {
+    services() {
       this.config.options = [];
       this.services.forEach((service) => {
-        this.config.options.push({ value: service.id });
+        this.config.options.push({ value: service.serviceName });
       });
       this.dropdownKey += 1;
     },
